1eb773f4f6fccc6c9fd3a762451216b8ed66650c
Chinedu Chaz <ccognate@gmail.com>
2024-06-24 10:41:53 +0100

    feat add websocket

--- App/Views/index.view.php
+++ App/Views/index.view.php
@@ -1,1 +1,33 @@
-Testing the view loading
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Websocket Test</title>
+    <script type="text/javascript" defer>
+        const conn = new WebSocket('ws://localhost:8080');
+        conn.onopen = (e)=>{
+            conn.send("I greet all")
+            console.log("connection establisted")
+        };
+
+        //receive message from the server
+        conn.onmessage=(e)=>{
+            console.log(e.data);
+        }
+
+    </script>
+</head>
+<body>
+    Purely php sockets
+    <input type="button" value="Send Request" id="send">
+<script>
+    const button =  document.getElementById("send");
+    button.addEventListener("click",()=>{
+        conn.send("button was clicked")
+    });
+</script>
+</body>
+</html>
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit02e64d85d8e754a1727491265e85f50b
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit02e64d85d8e754a1727491265e85f50b', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit02e64d85d8e754a1727491265e85f50b', 'loadClassLoader'));
@@ -31,6 +33,25 @@ class ComposerAutoloaderInit02e64d85d8e754a1727491265e85f50b
 
         $loader->register(true);
 
+        $includeFiles = \Composer\Autoload\ComposerStaticInit02e64d85d8e754a1727491265e85f50b::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire02e64d85d8e754a1727491265e85f50b($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire02e64d85d8e754a1727491265e85f50b($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
